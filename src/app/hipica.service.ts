192ad7af33ba01bf8f397381a02b5e3b0c7360e2
ItziarEspino <itziaespino19@iespuntadelverde.es>
2021-06-01 09:05:42 +0200

    first commit

--- src/app/hipica.service.ts
+++ src/app/hipica.service.ts
@@ -5,14 +5,14 @@ import { Competicion } from './models/Competicion';
 
 @Injectable({ providedIn: 'root' })
 export class HipicaService {
-  private url = 'http://localhost:3000/';
+  private url = 'https://proyectofinalapihipica.herokuapp.com/';
   constructor(private http: HttpClient) {}
   getAlumno(idAl: string) {
-    const urlget = `http://localhost:3000/alumno/${idAl}`;
+    const urlget = `https://proyectofinalapihipica.herokuapp.com/alumno/${idAl}`;
     return this.http.get(urlget);
   }
   addAlumno(doc: any) {
-    const urlpost = `http://localhost:3000/alumno`;
+    const urlpost = `https://proyectofinalapihipica.herokuapp.com/alumno`;
     return this.http.post(urlpost, doc);
   }
 
@@ -22,27 +22,27 @@ export class HipicaService {
   }
 
   updateAlumno(doc: any) {
-    const urlpost = `http://localhost:3000/alumno/${doc.idAl}`;
+    const urlpost = `https://proyectofinalapihipica.herokuapp.com/alumno/${doc.idAl}`;
     return this.http.post(urlpost, doc);
   }
 
   getCompeticion(idComp: number, nomComp: string) {
-    const urlget = `http://localhost:3000/competicion/${idComp}&${nomComp}`;
+    const urlget = `https://proyectofinalapihipica.herokuapp.com/competicion/${idComp}&${nomComp}`;
     return this.http.get(urlget);
   }
 
   addCompeticion(doc: any) {
-    const urlpost = `http://localhost:3000/competicion`;
+    const urlpost = `https://proyectofinalapihipica.herokuapp.com/competicion`;
     return this.http.post(urlpost, doc);
   }
 
   updateCompeticion(doc: any) {
-    const urlpost = `http://localhost:3000/competicion/${doc.idComp}&${doc.nomComp}`;
+    const urlpost = `https://proyectofinalapihipica.herokuapp.com/competicion/${doc.idComp}&${doc.nomComp}`;
     return this.http.post(urlpost, doc);
   }
 
   deleteAlumno(alumno: Alumno) {
-    const urlget = `http://localhost:3000/borrarAlumno/${alumno.idAl}&${alumno.nombr}`;
+    const urlget = `https://proyectofinalapihipica.herokuapp.com/borrarAlumno/${alumno.idAl}&${alumno.nombr}`;
     return this.http.get(urlget);
   }
 }
